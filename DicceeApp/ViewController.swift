6300e0bc1db754b4dbe729062997393fd86affb1
AechGG <hazg@me.com>
2020-08-04 12:21:49 +0100

    feat: change image on start up

--- DicceeApp/ViewController.swift
+++ DicceeApp/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // diceImageViewOne.image = UIImage(named: "DiceFour");
+        // OR use image literal
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceSix")
     }
 
 
